beb9aecfcea271f3afe7185206e077a794858fc0
1C3T0C0LD <113115886+1C3T0C0LD@users.noreply.github.com>
2022-11-24 15:36:59 +0000

    commit

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform myPlatform;
+    public Transform myStartPoint;
+    public Transform myEndPoint;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
